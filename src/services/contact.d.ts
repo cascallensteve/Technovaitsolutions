b207cfd08ff2f69b774033ce69b8a127f8fa159e
CascallenSteve <cascallensteve@gmail.com>
2025-11-06 16:41:04 +0300

    ?

--- src/services/contact.d.ts
+++ src/services/contact.d.ts
@@ -1,14 +1,12 @@
-declare module '../services/contact' {
-  export function createInquiry(payload: {
-    first_name: string;
-    last_name: string;
-    email: string;
-    message: string;
-    company?: string;
-  }): Promise<any>;
+export function createInquiry(payload: {
+  first_name: string;
+  last_name: string;
+  email: string;
+  message: string;
+  company?: string;
+}): Promise<any>;
 
-  export function listInquiries(): Promise<any[]>;
+export function listInquiries(): Promise<any[]>;
 
-  const _default: { createInquiry: typeof createInquiry; listInquiries: typeof listInquiries };
-  export default _default;
-}
+declare const _default: { createInquiry: typeof createInquiry; listInquiries: typeof listInquiries };
+export default _default;
